19d968a9a0314a4f5297edd5e9b023c2bb0b9791
davidgoulart-dev <davidgoulart.tae@gmail.com>
2024-07-18 16:12:31 -0300

    for em arrays

---
+++ meu-primeiro-projeto-java/src/ExemploForArray.java
@@ -0,0 +1,11 @@
+public class ExemploForArray {
+public static void main(String[] args) {
+    String alunos[] = {"Felipe", "João", "Marcio","Julia"};
+
+    for (int x=0; x<alunos.length; x++){
+        
+        System.out.println("Aluno no índice x=" + x +"é" + alunos[x]);
+    
+    }
+}
+}
